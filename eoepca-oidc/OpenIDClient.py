77c41837e56ecae3e48f91084bd922b64455f11b
AlvaroVillanueva <alvaro.villanueva@deimos-space.com>
2020-03-27 13:30:07 +0000

    EOEPCA-11-Flow-Tested-and-semi-doc

--- eoepca-oidc/OpenIDClient.py
+++ eoepca-oidc/OpenIDClient.py
@@ -1,31 +1,34 @@
 import requests
 from requests.exceptions import HTTPError
 class OpenIDClient:
-    def __init__(self, issuer = None, scope = None, acces_token = None, response_type=None, client_id=None, redirect_uri=None):
-        self._token={}
+    '''
+    implementation of an OIDC Client module in order to provide Client authentication and End-User authentication functionality according to the OpenIDConnect Authentication Code Flow
+        Input params:
+            <string>issuer: 
+            <string>authType: Bearer(token auth), Basic(client credentials) or None(New auth). It must be set when the user has login parameters.
+    
+    '''
+    def __init__(self, issuer = None, scope = None, acces_token = None, response_type=None, client_id=None, client_secret=None, redirect_uri=None, authType = None):
+        self._token = {}
         self.scope = scope
         self.redirect_uri = redirect_uri
         self._client_id = client_id
-        self.response_type = 'code'
-        self._client_secret = None
+        self.response_type = response_type
+        self._client_secret = client_secret
         self._code = None
         self._token['token_hint'] = acces_token
         self.method = 'GET'
         self.issuer = issuer
         self.state = None
+        self._authType= authType
     
     def authorized(self):
         '''
         Boolean method that returns true in case the client an authorization token
         '''
         try:
-                
             for k,v in _token:
-                print(bool(v))
-            a = bool(self._token['token_hint'])
-            print (self._token)
-            
-            print(bool(self._token))
+                a = bool(self._token['token_hint'])
             return bool(self._token)
         except Exception as err:
             return False
@@ -43,17 +46,18 @@ class OpenIDClient:
         else:
             raise Exception('Scope error, could not find the required scopes for OIDC Connect auth')
 
-    def requestAuth(self, method, issuer, token = None, verify = True):
+    def requestAuth(self, method, issuer, verify = True):
         '''
-        The logic implemented on this webpage should retrieve the token from the URL
+        The logic implemented on this webpage should retrieve the token from the URL in case the user is not authenticated
+        If a token_hint is given the OP should response a succesfully authentication
         '''
         try:
             uri_dict_supported, scope_list_supported = self.getDiscoveryUrl(issuer, verify)
-
+           
             if method == 'GET':
-                return self.getRequestCode(uri_dict,self.token_hint,verify)
+                return self.getRequestCode(uri_dict,self.token_hint, verify)
             elif method == 'POST':
-                return self.postRequestToken(uri_dict)
+                return self.postRequestToken(uri_dict,self.token_hint, verify)
             else:
                 raise Exception('The request must have a method defined')
         except Exception as err:
@@ -62,6 +66,9 @@ class OpenIDClient:
     def getDiscoveryUrl(self, sso_node, verify = True):
         '''
         Method that retrieves information from the openid configuration of the host by GET method. It is used to verify the scopes and endpoint inputs
+        Input
+            <String>sso_node: The domain of the issuer must be given
+            <Bool>verify: set as True by default in order to secure the connection against Man in the middle attacks
         Returns 
             <List>scope_list: List of all supported scopes by the host
             <Dict>url_dict: Dictionary with all host endpoints 
@@ -71,7 +78,6 @@ class OpenIDClient:
         response.encoding = 'utf-8'
         scope_list=[]
         url_list=[]
-        
         for k , v in response.json().items():
             if "scopes_supported" in k:
                 scope_list=v
@@ -83,45 +89,63 @@ class OpenIDClient:
                 continue
         return url_dict, scope_list
 
-    def getRequestCode(self, uri_list, token = False, verify=True):
+    def getRequestCode(self, uri_list, token = None, verify=True):
         '''
-        Method that retrieves information from the authorization endpoint in order to retrieve the authorization code   
+        Method that retrieves information from the authorization endpoint in order to retrieve the authorization code
+        The response of the get request is parsed in order to retrieve the authentication code  
         '''
-        header = None
+        headers = None
         if token:
-            header = {'authorization': "Basic "+str(token)}
-        provider_config={"scope": self.scope,"response_type": 'code', "client_id": self.client_id,"redirect_uri": self.redirect_uri}
+            headers = {'authorization': self._authType+str(token)}
+            provider_config={"scope": self.scope,"response_type": 'code', "client_id": self.client_id,"redirect_uri": self.redirect_uri,"prompt":'login'}
+        else:
+            provider_config={"scope": self.scope,"response_type": 'code', "client_id": self.client_id,"redirect_uri": self.redirect_uri}
         try:
-            response=requests.get(uri_list["authorization_endpoint"], data=provider_config, header = header, verify=verify)
+            response=requests.get(uri_list["authorization_endpoint"], data=provider_config, headers = headers, verify=verify)
             response.encoding = 'utf-8'
-            self._code = self.retrieveCode(response.json())
+
+            self._code = self.retrieveCode(response.content)
         except HTTPError as http_error_msg:
             raise Exception('HTTP error occurred: '+str(response.status_code)+': ' + str({http_error_msg}))
         except Exception as err:
             raise Exception('Other error occurred: '+ str({err}))
         
     def retrieveCode(self, response):
+        '''
+        The input parameter content is parsed in order to retrieve and return the authentication code
+        '''
         code = response.split('code=')[-1].split('&')
         return code[0]
 
-    def postRequestToken(self,uri_list):
+    def postRequestToken(self,uri_list, token = None, verify=True):
         '''
         Method that retrieves information from the token endpoint in order to retrieve the authorization token 
         '''
-        provider_config={"grant_type": 'authorization_code', "code": self._code, "redirect_uri": self.redirect_uri, "scope": self.scope, "client_id": self.client_id, "client_secret": self.client_secret}
-        headers = { 'content-type': "application/x-www-form-urlencoded" }
+        headers = None
+        if token:
+            headers = {'authorization': self._authType+str(token),'content-type': "application/x-www-form-urlencoded" }
+            provider_config={"grant_type": 'client_credentials'}
+        else: 
+            headers = { 'content-type': "application/x-www-form-urlencoded" }
+
+            provider_config={"grant_type": 'authorization_code', "code": self._code, "redirect_uri": self.redirect_uri, "scope": self.scope, "client_id": self.client_id, "client_secret": self.client_secret}
+        
         if self.client_id and self.client_secret:
             try:
-                response = requests.post(uri_list["token_endpoint"], data=provider_config, headers=headers)
+                response = requests.post(uri_list["token_endpoint"], data=provider_config, headers=headers, verify = verify)
                 response.encoding = 'utf-8'
-                self.retrieveToken(response.json().items())
-                response.raise_for_status()
+                #set token dictionary with the response values
+                self._token=self.retrieveToken(response.json().items())
             except HTTPError as http_error_msg:
                 raise Exception('HTTP error occurred: ' + str({http_error_msg}))
             except Exception as err:
                 raise Exception('Other error occurred: '+ str({err}))
             
     def retrieveToken(self, response):
+        '''
+        The retrieved token will be set as dictionary where the keys are the token parameters recived in the body
+
+        '''
         tkn = {}
         for k , v in response:
             if "access_token" in k:
@@ -136,36 +160,16 @@ class OpenIDClient:
                 tkn[k]=v
             else:
                 continue
-        self._token=tkn
-  
-
-    # def connectWithApi(self, url_dict):
-    #     if self.authorized:
-    #         headers = {'content-type': "application/x-www-form-urlencoded",'authorization': "Bearer "+ self._token}
-    #         try:
-    #             response = requests.get(uri_dict["issuer"], headers=headers)
-    #             response.encoding = 'utf-8'
-    #             token = {}
-    #             self.retrieveToken(response.json().items())
-    #             response.raise_for_status()
-    #         except HTTPError as http_error_msg:
-    #             print('HTTP error occurred: ' + str({http_error_msg}))
-    #         except Exception as err:
-    #             print('Other error occurred: '+ str({err}))
-    #         else:
-    #             print('Success!')
-    #     else:
-    #         print('Not authorized with token')
-    #         pass
+        return tkn
 
     def validateToken():
         '''
         Method that retrieves information from the authorization endpoint in order to retrieve the authorization code 
+        In order to validate the recived token, some steps must be accomplished according to Authentication Flow.
+        In the current usage stage there is no need to validate it as it's only needed the acces_token
         '''
-        pass
         
-    def testLib(self):
-        print('Lets do some requests')
+        pass
 
     @property
     def client_id(self): 
@@ -198,4 +202,12 @@ class OpenIDClient:
     @token.setter 
     def token(self, a): 
         self._token = a 
+    
+    @property
+    def authType(self): 
+        return self._authType
+         
+    @authType.setter 
+    def authType(self, a): 
+        self._authType = a 
   
--- eoepca-oidc/tests/sum_test.py
+++ eoepca-oidc/tests/sum_test.py
@@ -8,38 +8,63 @@ import os, sys
 sys.path.append(os.path.abspath('..'))
 from OpenIDClient import OpenIDClient
 
-def mocked_requests_get(*args, **kwargs):
+def mocked_requests_post(*args, **kwargs):
     class MockResponse:
-        def __init__(self, json_data, status_code):
+        def __init__(self, content ,json_data, status_code):
             self.json_data = json_data
             self.status_code = status_code
+            self.content = content
 
         def json(self):
             return self.json_data
+        def content(self):
+            return self.content
+    obj = {"access_token": "SlAV32hkKG", "token_type": "Bearer", "refresh_token": "8xLOxBtZp8", "expires_in": 3600, "id_token": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjFlOWdkazcifQ.ewogImlzcyI6ICJodHRwOi8vc2VydmVyLmV4YW1wbGUuY29tIiwKICJzdWIiOiAiMjQ4Mjg5NzYxMDAxIiwKICJhdWQiOiAiczZCaGRSa3F0MyIsCiAibm9uY2UiOiAibi0wUzZfV3pBMk1qIiwKICJleHAiOiAxMzExMjgxOTcwLAogImlhdCI6IDEzMTEyODA5NzAKfQ.ggW8hZ1EuVLuxNuuIJKX_V8a_OMXzR0EHR9R6jgdqrOOF4daGU96Sr_P6qJp6IcmD3HP99Obi1PRs-cwh3LO-p146waJ8IhehcwL7F09JdijmBqkvPeB2T9CJNqeGpe-gccMg4vfKjkM8FcGvnzZUN4_KSP0aAp1tOJ1zZwgjxqGByKHiOtX7TpdQyHE5lcMiKPXfEIQILVq0pc_E2DzL7emopWoaoZTF_m0_N0YzFC6g6EJbOEoRoSK5hoDalrcvRYLSrQAZZKflyuVCyixEoV9GfNQC3_osjzw2PAithfubEEBLuVVk4XUVrWOLrLl0nx7RkKU8NXNHq-rvKMzqg"}
+    obj2={"access_token":"2YotnFZFEjr1zCsicMWpAA","token_type":"example","expires_in":3600,"example_parameter":"example_value"}
+    if 'application/x-www-form-urlencoded' in list(kwargs.get('headers').items())[0] and 'code' in kwargs.get('data') and 'client_id' in kwargs.get('data') and 'client_secret' in kwargs.get('data') and 'redirect_uri' in kwargs.get('data'):
+        return MockResponse('HTTP/1.1 200 OK Content-Type: application/json Cache-Control: no-store Pragma: no-cache ',obj, 200)
+    elif 'authorization' in list(kwargs.get('headers').items())[0] and 'client_credentials' in list(kwargs.get('data').items())[0]:
+        return MockResponse('HTTP/1.1 200 OK Content-Type: application/json;charset=UTF-8 Cache-Control: no-store Pragma: no-cache ',obj2, 200)
+    return MockResponse(None, 404)
+
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, content ,json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+            self.content = content
+
+        def json(self):
+            return self.json_data
+        def content(self):
+            return self.content
+
     with open('tst.json', 'r') as myfile:
         data=myfile.read()
     obj = json.loads(data)
-    print(type(obj))
-    for i  in kwargs:
-        print(kwargs.get(i))
+    
+    # add json data if provided
     if '/.well_known/openid-configuration' in args[0]:
-        return MockResponse(obj, 200)
-    elif kwargs.get('header') and 'scope' in kwargs.get('data') and 'response_type' in kwargs.get('data') and 'client_id' in kwargs.get('data') and 'redirect_uri' in kwargs.get('data'):
-        return MockResponse('HTTP/1.1 302 Found Location: https://client.example.org/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=af0ifjsldkj', 200)
+        return MockResponse(None,obj, 200)
+    elif kwargs.get('headers') and 'scope' in kwargs.get('data') and 'response_type' in kwargs.get('data') and 'client_id' in kwargs.get('data') and 'redirect_uri' in kwargs.get('data'):
+        return MockResponse('HTTP/1.1 302 Found Location: https://client.example.org/cb?code=SplxlOBeZQQYbYS6WxSbIA&state=af09ure9urf',None, 200)
+    elif not kwargs.get('headers') and 'scope' in kwargs.get('data') and 'response_type' in kwargs.get('data') and 'client_id' in kwargs.get('data') and 'redirect_uri' in kwargs.get('data'):
+        return MockResponse('HTTP/1.1 302 Found Location: https://client.example.org/cb?code=AIIEHGSKIUOIFNKLOSIUOI&state=af09ure9urf',None, 200)
     return MockResponse(None, 404)
 
 
 
 class OIDC_Unit_Test(unittest.TestCase):
+    
 
     # First, mock has to simulate http response, therefore a patch of get and post methods from requests library will be needed.
+    
     @mock.patch('requests.get', side_effect=mocked_requests_get)
-    def test_fetch(self, mock_get,raise_for_status=None):
+    def test_get(self, mock_get,raise_for_status=None):
         mock_resp = mock.Mock()
         mock_resp.raise_for_status = mock.Mock()
         if raise_for_status:
             mock_resp.raise_for_status.side_effect = raise_for_status
-        
         oidc = OpenIDClient()
         oidc.scope = 'openid'
         url_list={'issuer': '[APIDOMAIN]', 'authorization_endpoint': '[APIDOMAIN]/oxauth/restv1/authorize', 'token_endpoint': '[APIDOMAIN]/oxauth/restv1/token', 'userinfo_endpoint': '[APIDOMAIN]/oxauth/restv1/userinfo', 'clientinfo_endpoint': '[APIDOMAIN]/oxauth/restv1/clientinfo', 'end_session_endpoint': '[APIDOMAIN]/oxauth/restv1/end_session', 'registration_endpoint': '[APIDOMAIN]/oxauth/restv1/register', 'id_generation_endpoint': '[APIDOMAIN]/oxauth/restv1/id'}
@@ -51,12 +76,35 @@ class OIDC_Unit_Test(unittest.TestCase):
         #check the input parameters to get request
         self.assertIn(mock.call('[APIDOMAIN]/.well_known/openid-configuration',verify=True), mock_get.call_args_list)
 
-        provider_config={"scope": 'openid',"response_type": 'code', "client_id": 'randomClient',"redirect_uri": 'http://url/callback'}
+        #provider_config={"scope": 'openid',"response_type": 'code', "client_id": 'randomClient',"redirect_uri": 'http://url/callback'}
+        oidc.getRequestCode(uris, verify=False)        
+        self.assertEqual(oidc._code, 'AIIEHGSKIUOIFNKLOSIUOI')
+        oidc.authType='Bearer '
         oidc.getRequestCode(uris,token = 'aslf;alksjkekeke', verify=False)
         self.assertEqual(oidc._code, 'SplxlOBeZQQYbYS6WxSbIA')
-        
-       
+
     
+    @mock.patch('requests.post', side_effect=mocked_requests_post)
+    def test_fetch3(self, mock_get,raise_for_status=None):
+        mock_resp = mock.Mock()
+        mock_resp.raise_for_status = mock.Mock()
+        if raise_for_status:
+            mock_resp.raise_for_status.side_effect = raise_for_status
+        oidc = OpenIDClient()
+        oidc.scope = 'openid'
+        oidc._code = 'SplxlOBeZQQYbYS6WxSbIA'
+        oidc.client_id = 'Default client'
+        oidc.client_secret= 'itsASecret'
+        oidc.method = 'POST'
+        url_list={'issuer': '[APIDOMAIN]', 'authorization_endpoint': '[APIDOMAIN]/oxauth/restv1/authorize', 'token_endpoint': '[APIDOMAIN]/oxauth/restv1/token', 'userinfo_endpoint': '[APIDOMAIN]/oxauth/restv1/userinfo', 'clientinfo_endpoint': '[APIDOMAIN]/oxauth/restv1/clientinfo', 'end_session_endpoint': '[APIDOMAIN]/oxauth/restv1/end_session', 'registration_endpoint': '[APIDOMAIN]/oxauth/restv1/register', 'id_generation_endpoint': '[APIDOMAIN]/oxauth/restv1/id'}
+         #POST example of access token retrieval. 
+        oidc.postRequestToken(url_list, token = None, verify=True)
+        self.assertEqual(oidc._token['access_token'], 'SlAV32hkKG')
+        oidc.authType='Bearer '
+        oidc.postRequestToken(url_list, token = 'SlAV32hkKG', verify=True)
+        self.assertEqual(oidc._token['access_token'], '2YotnFZFEjr1zCsicMWpAA')
+       
+
 if __name__ == '__main__':
     unittest.main()
 
